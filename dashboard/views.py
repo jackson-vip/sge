5bfcce7c7d7d8a7200d9caad644040609bc29b11
Jackson_vip <jackson.tiburtinopb@gmail.com>
2025-04-27 16:02:09 -0300

    Adiciona breadcrumb ao layout do dashboard e estiliza o componente de breadcrumb

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse
 from django.views import View
 from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
@@ -12,3 +13,11 @@ class DashboardView(View):
 @method_decorator(login_required, name='dispatch')
 class DashboardView(TemplateView):
     template_name = 'dashboard/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Dashboard'
+        context['breadcrumbs'] = [
+            {'name': 'Dashboard', 'url': reverse('dashboard:home')},
+        ]
+        return context
