1fcf4c2103f48222345cb58ed90e46af31f7da2d
Jackson_vip <jackson.tiburtinopb@gmail.com>
2025-05-07 16:17:04 -0300

    Adiciona seções de estoque e movimentações no dashboard, incluindo tabelas para exibir produtos, movimentações recentes e estoque crítico.

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,10 +5,10 @@ from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
-# Create your views here.
+from estoque.models import MovimentacaoEstoque
+from produtos.models import Produto
 
-class DashboardView(View):
-    ...
+# Create your views here.
 
 @method_decorator(login_required, name='dispatch')
 class DashboardView(TemplateView):
@@ -20,4 +20,14 @@ class DashboardView(TemplateView):
         context['breadcrumbs'] = [
             {'name': 'Dashboard', 'url': reverse('dashboard:dashboard')},
         ]
+        # Estoque atual
+        context['produtos'] = Produto.objects.all()
+
+        # Movimentações recentes
+        context['movimentacoes'] = MovimentacaoEstoque.objects.order_by('-data_movimentacao')[:10]
+
+        # Estoque crítico (quantidade menor que 10)
+        context['estoque_critico'] = Produto.objects.filter(quantidade__lt=10)
+
+
         return context
