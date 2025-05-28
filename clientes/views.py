e2f6519b44ac8db3ca3c25382cf950653119571e
jackson_vip <jackson.tiburtinopb@gmail.com>
2025-05-28 14:10:44 -0300

    Refatora a visualização de clientes: substitui a classe de visualização para ListView, implementa paginação e melhora a filtragem de resultados.

--- clientes/views.py
+++ clientes/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
-from django.urls import reverse
-from django.views import View
+from django.views.generic import ListView, CreateView
+from django.db.models import Q
+from django.urls import reverse, reverse_lazy
 
 # Importação dos modelos
 from .models import Cliente
@@ -9,34 +9,55 @@ from .models import Cliente
 
 # Create your views here.
 
-class ClienteListView(LoginRequiredMixin, View):
+PRE_PAGES = 12
+
+class ClienteListView(LoginRequiredMixin, ListView):
+    model = Cliente
     template_name = 'cliente/cliente_list_view.html'
+    context_object_name = 'clientes'
+    paginate_by = PRE_PAGES
+
+    def get(self, request, *args, **kwargs):
+        if request.GET.get('opcao'):
+            self.paginate_by = request.GET.get('opcao')
+        return super().get(request, *args, **kwargs)
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        qs = self.request.GET.get('qs', '').strip()  # Use strip para remover espaços em branco
+
+        if qs:
+            queryset = queryset.filter(
+                Q(usuario__first_name__icontains=qs) |
+                Q(usuario__last_name__icontains=qs) |
+                Q(usuario__username__icontains=qs) |
+                Q(cpf__icontains=qs) |
+                Q(email__icontains=qs)
+            )
+        
+        return queryset
 
     def get_context_data(self, **kwargs):
-        context = {}
+        context = super().get_context_data(**kwargs)
         context['title'] = 'Clientes'
+        qs = self.request.GET.get('qs', '').strip()
+        context['qs'] = qs
         context['breadcrumbs'] = [
             {'name': 'Clientes', 'url': reverse('cliente:cliente_list_view')},
         ]
-        context['clientes'] = Cliente.objects.all()
         return context
 
-    def get(self, request, *args, **kwargs):
-        context = self.get_context_data()
-        return render(request, self.template_name, context)
-
-class ClienteCreateView(LoginRequiredMixin, View):
+class ClienteCreateView(LoginRequiredMixin, CreateView):
+    model = Cliente
     template_name = 'cliente/cliente_create_view.html'
+    fields = ['usuario', 'imagem']  # Substitua pelos campos do modelo Cliente
+    success_url = reverse_lazy('cliente:cliente_list_view')
 
     def get_context_data(self, **kwargs):
-        context = {}
+        context = super().get_context_data(**kwargs)
         context['title'] = 'Criar Cliente'
         context['breadcrumbs'] = [
-            {'name': 'Clientes', 'url': reverse('authentication:cliente_list')},
-            {'name': 'Criar Cliente', 'url': reverse('authentication:cliente_create')},
+            {'name': 'Clientes', 'url': reverse('cliente:cliente_list_view')},
+            {'name': 'Criar Cliente', 'url': reverse('cliente:cliente_create_view')},
         ]
         return context
-    
-    def get(self, request, *args, **kwargs):
-        context = self.get_context_data()
-        return render(request, self.template_name, context)
