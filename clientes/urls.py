e544856b48bd2e4f7debc4c0f95f5659ba1d1fc8
jackson_vip <jackson.tiburtinopb@gmail.com>
2025-05-29 00:25:57 -0300

    Adiciona rota para a visualização de deleção de clientes

--- clientes/urls.py
+++ clientes/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (
-    ClienteListView, ClienteCreateView,
+    ClienteListView, ClienteCreateView, ClienteDeleteView,
 )
 
 app_name = 'cliente'
@@ -9,6 +9,6 @@ urlpatterns = [
     path('', ClienteListView.as_view(), name='cliente_list_view'),
     path('novo/', ClienteCreateView.as_view(), name='cliente_create_view'),
     # path('<int:pk>/editar/', ClienteUpdateView.as_view(), name='update'),
-    # path('<int:pk>/deletar/', ClienteDeleteView.as_view(), name='delete'),
+    path('cliente_deletar/<int:pk>/', ClienteDeleteView.as_view(), name='cliente_delete_view'),
     # path('<int:pk>/', ClienteDetailView.as_view(), name='detail'),
 ]
