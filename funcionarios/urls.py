f9c0fbdfeb33d0d72a9ba7c2b7acfe9c08865749
jackson_vip <jackson.tiburtinopb@gmail.com>
2025-06-30 11:06:10 -0300

    adiciona rota para criação de funcionário no arquivo de URLs

--- funcionarios/urls.py
+++ funcionarios/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ( FuncionarioListView )
+from .views import ( FuncionarioListView, FuncionarioCreateView )
 
 app_name = 'funcionario'
 
 urlpatterns = [
     path('', FuncionarioListView.as_view(), name='funcionario_list_view'),
+    path('novo_funcionario/', FuncionarioCreateView.as_view(), name='funcionario_create_view'),
 ]
