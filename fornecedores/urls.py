b1cb1e6390fe497ffde58738174a56e018f1cb2b
Jackson Ferreira <jackson.tiburtinopb@gmail.com>
2026-03-09 21:33:34 -0300

    adiciona rotas para visualização, criação, edição e exclusão de fornecedores

--- fornecedores/urls.py
+++ fornecedores/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (
-    FornecedorListView, FornecedorCreateView,
+    FornecedorListView, FornecedorCreateView, FornecedorDeleteView, FornecedorDetailView, FornecedorUpdateView
 )
 
 app_name = 'fornecedor'
@@ -8,4 +8,7 @@ app_name = 'fornecedor'
 urlpatterns = [
     path('', FornecedorListView.as_view(), name='fornecedor_list_view'),
     path('novo_fornecedor/', FornecedorCreateView.as_view(), name='fornecedor_create_view'),
+    path('deletar_fornecedor/<int:pk>/', FornecedorDeleteView.as_view(), name='fornecedor_delete_view'),
+    path('detalhes_fornecedor/<int:pk>/', FornecedorDetailView.as_view(), name='fornecedor_detail_view'),
+    path('editar_fornecedor/<int:pk>/', FornecedorUpdateView.as_view(), name='fornecedor_update_view'),
 ]
